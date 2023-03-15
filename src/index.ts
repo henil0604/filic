ae034431704c17a44473b9681fbd9b011c626556
Henil Malaviya <henilmalaviya06@gmail.com>
2023-03-15 18:11:39 +0530

    exporting EntityTypes

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import Filic from '@/Filic.js';
 import File from '@/File';
 import Directory from '@/Directory';
 import Entity from '@/Entity';
+import { EntityTypes } from '@/types/Filic';
+
 
 export {
     File,
     Directory,
-    Entity
+    Entity,
+    EntityTypes
 }
 
 export default Filic;
